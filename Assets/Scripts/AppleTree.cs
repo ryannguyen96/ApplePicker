8503ab414c2d44f37bfca34791791a1c7b31cde2
Ryan Nguyen <shiga2714@gmail.com>
2018-09-21 02:11:03 -0400

    Implemented AppleTree script
    - AppleTree script was implemented.
    - Tree position slightly adjusted.
    - AppleTree script is tied to AppleTree prefab.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+    
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+    
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        // Dropping apples every second
+    }
+    void Update()
+    {
+        // Basic Movement
+        // Changing Direction
+    }
 }
